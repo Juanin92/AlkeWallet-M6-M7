243df7eeb1994c6cdf3bc114d9b7739aa6d48155
Juanin92 <j.claveria19@com>
2024-05-25 19:40:09 -0400

    entity account

--- src/main/java/com/model/Account.java
+++ src/main/java/com/model/Account.java
@@ -2,7 +2,6 @@ package com.model;
 
 import jakarta.persistence.*;
 import lombok.*;
-import lombok.extern.apachecommons.CommonsLog;
 
 @Entity
 @Table(name = "accounts")
--- src/main/java/com/repository/AccountRepository.java
+++ src/main/java/com/repository/AccountRepository.java
@@ -1,7 +1,6 @@
 package com.repository;
 
 import com.model.Account;
-import com.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
